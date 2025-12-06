014450ccb320e4956fbb7206895a6e749ad4fb76
phatlt17420 <phatlt17420@gmail.com>
2025-12-06 11:15:53 +0700

    06/12/2025

---
+++ c01_Toi_T6_Chieu_T7/BaiTapTrenLop/KeoBuaBao.py
@@ -0,0 +1,35 @@
+nguoi = input('Người ra: ')
+while (nguoi not in ['kéo', 'búa', 'bao']):
+    nguoi = input('Người nhập lại: ')
+
+import random as r
+
+may = r.choice(['kéo', 'búa', 'bao'])  # choices : tạo ra nhiều giá trị; choice: chọn  giá trị
+print('Máy ra: ', may)
+if nguoi == may:
+    print('Hoà')
+elif (nguoi == 'bao' and may == 'búa') or \
+        (nguoi == 'kéo' and may == 'bao') or \
+        (nguoi == 'búa' and may == 'kéo'):
+    print('Người thắng máy')
+else:
+    print('Người thua máy')
+
+# if nguoi=='kéo' and may=='búa' :
+#     print('Người thua máy')
+# elif nguoi=='kéo' and may=='bao' :
+#     print('Người thắng máy')
+# elif nguoi=='búa' and may=='bao' :
+#     print('Người thua máy')
+# elif nguoi=='búa' and may=='kéo' :
+#     print('Người thắng máy')
+# elif nguoi=='bao' and may=='kéo' :
+#     print('Người thua máy')
+# elif nguoi == 'bao' and may == 'bao':
+#     print('Hoà')
+# elif nguoi == 'kéo' and may == 'kéo':
+#     print('Hoà')
+# elif nguoi == 'búa' and may == 'búa':
+#     print('Hoà')
+# else: #nguoi=='bao' and may=='búa'
+#     print('Người thắng máy')
